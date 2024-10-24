8dfb6e74d23d87346643846bfacf456e89781c58
Melis Filiz Esenkaya <127357851+Filiz5@users.noreply.github.com>
2024-10-24 23:47:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
-  # access ve secret
   region = "us-east-1"
 }
+
 variable "key" {
   default = "jenkins-project"
 }
@@ -20,18 +19,39 @@ variable "user" {
   default = "techpro"
 }
 
+# IAM Role ve Instance Profile Oluşturma
+resource "aws_iam_role" "jenkins_project_role" {
+  name = "jenkins-project-role-${var.user}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "jenkins_project_instance_profile" {
+  name = "jenkins-project-profile-${var.user}"
+  role = aws_iam_role.jenkins_project_role.name
+}
+
+# EC2 Instance Oluşturma
 resource "aws_instance" "managed_nodes" {
   ami = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
   key_name = var.key
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  iam_instance_profile = "jenkins-project-profile-${var.user}"
+  iam_instance_profile = aws_iam_instance_profile.jenkins_project_instance_profile.name
   tags = {
     Name = "jenkins_project"
   }
 }
 
-
+# Security Group
 resource "aws_security_group" "tf-sec-gr" {
   name = "project-jenkins-sec-gr"
   tags = {
@@ -71,6 +91,7 @@ resource "aws_security_group" "tf-sec-gr" {
   }
 }
 
+# EC2 Instance'ın IP'sini Çıktı Olarak Verme
 output "node_public_ip" {
   value = aws_instance.managed_nodes.public_ip
 }
